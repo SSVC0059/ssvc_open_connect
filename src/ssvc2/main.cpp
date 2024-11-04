be5eefe9df5f458c089fa74a537bdd92dfb613de
thedemoncat <nl.ruslan@yandex.ru>
2024-11-05 00:09:21 +0600

    Подключение svelte

--- src/ssvc2/main.cpp
+++ src/ssvc2/main.cpp
@@ -5,6 +5,7 @@
 #include "Logger.h"
 #include <ESPAsyncWebServer.h>
 #include "./TelemetryHandler/TelemetryHandler.h"
+#include "svelteesp32.h"
 
 
 Connector* connector;
@@ -40,32 +41,9 @@ void setup() {
 
     connector->sendCommandGetSettings();
 
-    // Определение маршрута для корневого URL
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-        // HTML с эмуляцией SSE на клиенте
-        request->send(200, "text/html", R"rawliteral(
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <title>SSE Example</title>
-            </head>
-            <body>
-                <h1>Server-Sent Events (SSE) Example</h1>
-                <p id="sse-data">Waiting for events...</p>
-                <script>
-                    const eventSource = new EventSource('/events');
-                    eventSource.onmessage = function(event) {
-                        const sseData = document.getElementById('sse-data');
-                        sseData.innerHTML = event.data;
-                    };
-                </script>
-            </body>
-            </html>
-        )rawliteral");
-    });
-
     /// Инициализация события
     server.addHandler(&events);
+    initSvelteStaticFiles(&server);
     server.begin();
 
 
